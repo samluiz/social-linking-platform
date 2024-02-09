e9eb64cc884f3585acedc049b23c4fa4a39379b4
samluiz <samuelluizrl@outlook.com>
2024-02-09 02:10:46 -0300

    fix error handling

--- src/components/react/Songs.tsx
+++ src/components/react/Songs.tsx
@@ -1,4 +1,3 @@
-import { Suspense } from 'react';
 import { useSpotifyToken } from './hooks/useSpotifyToken';
 import useSpotifyTracks, { type Track } from './hooks/useSpotifyTracks';
 
@@ -26,16 +25,14 @@ function Songs() {
 
   return (
     <>
+    {!tokenLoading && !tracksLoading && tracks && Array.isArray(tracks) ? (
     <div className='max-w-[1668px] grid place-items-center p-6'>
       <h1 className='sm:text-4xl text-2xl text-white py-2'>TOP TRACKS</h1>
-      <Suspense fallback={<p>Loading...</p>}>
-        {!tokenLoading && !tracksLoading && tracks && Array.isArray(tracks) ? (
           <TrackList tracks={tracks} />
-        ) : error ? (
-          <p>Error loading tracks: {error}</p>
-        ) : null}
-      </Suspense>
-    </div>
+    </div>)
+         : error ? (
+          null
+        ) : null }
     </>
   );
 }
